da277933dd3b56e8fa219164bbdddaa956306765
Martin Fabrizzio Vilche <mfvilche@gmail.com>
2020-04-16 14:41:12 -0300

    Update HelloWorldEndpoint.java

--- src/main/java/com/damdamdeo/helloworld/rest/HelloWorldEndpoint.java
+++ src/main/java/com/damdamdeo/helloworld/rest/HelloWorldEndpoint.java
@@ -12,7 +12,7 @@ public class HelloWorldEndpoint {
 	@GET
 	@Produces("text/plain")
 	public Response doGet() {
-		return Response.ok("Hello from Thorntail!").build();
+		return Response.ok("ES UN DEMO!").build();
 	}
 }
 
